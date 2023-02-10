81d61bc2abb413fdc44933450dd860caf39da654
K-DoYeon <loveworud9@gmail.com>
2023-02-10 17:59:09 +0900

    0210

---
+++ javascript/0210/test.js
@@ -0,0 +1,12 @@
+let phoneNum = "010-1234-5678";
+let result_1 = phoneNum.substring(phoneNum.length-4, 0);
+console.log(result_1,"****");
+
+let imgSrc = "images/but_out.jpg";
+let result_2 = imgSrc.replace("out.jpg","over.jpg");
+console.log(result_2);
+
+// const menu = ["짜장면", "돈가스", "된장국", "김치찌개", "회덮밥"];
+// let a = Math.floor(Math.random() * menu.length);
+// console.log(menu[a]);
+
